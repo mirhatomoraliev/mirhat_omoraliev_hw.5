58ff00803531687ba99cd82f3582b0e2d188b0b0
mirhatomoraliev <mirhatomoraliev@gmail.com>
2023-01-22 23:24:06 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mirhat_omoraliev_hw5/MainActivity.java
@@ -0,0 +1,22 @@
+ package com.example.mirhat_omoraliev_hw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+ public class MainActivity extends AppCompatActivity {
+
+     @SuppressLint("MissingInflatedId")
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
